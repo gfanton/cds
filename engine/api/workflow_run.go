a2d7efb32724599de16b288303238f7126348083
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2019-02-06 13:08:52 +0100

    ut
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/api/workflow_run.go
+++ engine/api/workflow_run.go
@@ -1033,6 +1033,7 @@ func (api *API) getDownloadArtifactHandler() service.Handler {
 		w.Header().Add("Content-Type", "application/octet-stream")
 		w.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", art.Name))
 
+		// TODO YESNAULT
 		f, err := api.SharedStorage.Fetch(art)
 		if err != nil {
 			_ = f.Close()
@@ -1082,6 +1083,8 @@ func (api *API) getWorkflowRunArtifactsHandler() service.Handler {
 				wg.Add(1)
 				go func(a *sdk.WorkflowNodeRunArtifact) {
 					defer wg.Done()
+
+					// TODO YESNAULT
 					s, temporaryURLSupported := api.SharedStorage.(objectstore.DriverWithRedirect)
 					if temporaryURLSupported { // with temp URL
 						fURL, _, err := s.FetchURL(a)
